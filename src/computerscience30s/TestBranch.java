bca6ec5a4c5ea3298d5d137df72b3fb8ef150172
Mr. Wachs <mrLWachs@gmail.com>
2018-09-06 08:22:17 -0500

    New 2018-2019 School Year

---
+++ src/computerscience30s/TestBranch.java
@@ -0,0 +1,21 @@
+/*
+ *  Mr. Wachs' classes - Java Computer Science learning file TestBranch.java
+ *  for project ComputerScience30S on 6-Sep-2018 at 8:22:02 AM by lawrence.wachs
+ */
+
+
+/** required package class namespace */
+package computerscience30s;
+
+
+
+/**
+ * TestBranch.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 6-Sep-2018 
+ */
+public class TestBranch 
+{
+
+}
